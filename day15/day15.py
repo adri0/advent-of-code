ff56a1ef23670293fab37a4db0a20f9a23ee0a5e
Adriano Oliveira <8104069+adri0@users.noreply.github.com>
2022-12-24 11:29:55 +0100

    Day15 part 1

--- day15/day15.py
+++ day15/day15.py
@@ -1,7 +1,11 @@
-from functools import reduce
+from functools import reduce, partial
 from dataclasses import dataclass
+from typing import NamedTuple
 
-Point = tuple[int, int]
+
+class Point(NamedTuple):
+    x: int
+    y: int
 
 
 @dataclass
@@ -11,73 +15,63 @@ class Sensor:
     reach: int
 
 
-def parse_beacons_input(input_path) -> dict[Point, Point]:
-    sensors_to_beacon = {}
+def distance(a: Point, b: Point) -> int:
+    return abs(b.x - a.x) + abs(b.y - a.y)
+
+
+def parse_sensor_data(input_path: str) -> list[Sensor]:
+    sensors = []
     for line in open(input_path):
         part1, part2 = line.strip().split(":")
-        x_s = int(part1[part1.find("x=")+2 : part1.find(",")])
-        y_s = int(part1[part1.find("y=")+2 :])
-        x_b = int(part2[part2.find("x=")+2 : part2.find(",")])
-        y_b = int(part2[part2.find("y=")+2 :])
-        sensor = (x_s, y_s)
-        beacon = (x_b, y_b)
-        sensors_to_beacon[sensor] = beacon
-    return sensors_to_beacon
+        x_sen = int(part1[part1.find("x=")+2 : part1.find(",")])
+        y_sen = int(part1[part1.find("y=")+2 :])
+        x_bea = int(part2[part2.find("x=")+2 : part2.find(",")])
+        y_bea = int(part2[part2.find("y=")+2 :])
+        sensor_pos = Point(x_sen, y_sen)
+        beacon = Point(x_bea, y_bea)
+        sensor = Sensor(
+            pos=sensor_pos, 
+            beacon=beacon, 
+            reach=distance(sensor_pos, beacon)
+        )
+        sensors.append(sensor)      
+    return sensors
 
 
-def distance(a: Point, b: Point) -> int:
-    x_a, y_a = a
-    x_b, y_b = b
-    return abs(x_b - x_a) + abs(y_b - y_a)
-
-
-def get_sensors_reach(sensors_to_beacon: dict[Point, Point]) -> dict[Point, int]:
-    return {
-        sensor: distance(sensor, beacon) 
-        for sensor, beacon in sensors_to_beacon.items()
-    }
-
-
-def coverage_in_row_by_sensor(y_row: int, sensor: Point, reach: int) -> set[Point]:
-    x_sensor = sensor[0]
-    mid_point = (x_sensor, y_row)
-    dist_mid_point = distance(sensor, mid_point)
-    if dist_mid_point > reach:
+def row_coverage_by_sensor(y_row: int, sensor: Sensor) -> set[Point]:
+    mid_point = Point(sensor.pos.x, y_row)
+    dist_mid_point = distance(sensor.pos, mid_point)
+    if dist_mid_point > sensor.reach:
         return set()
-    row_reach = reach - dist_mid_point 
+    row_reach = sensor.reach - dist_mid_point 
     return {
-        (x, y_row) 
-        for x in range(x_sensor - row_reach, x_sensor + row_reach + 1)
+        Point(x, y_row)
+        for x in range(sensor.pos.x - row_reach, sensor.pos.x + row_reach + 1)
     }
 
 
-def count_points_covered_in_row(y_row, sensors_reach):
+def count_points_covered_in_row(y_row: int, sensors: list[Sensor]) -> int:
     return len(
         reduce(
-            lambda set1, set2: set1.union(set2), 
-            (
-                coverage_in_row_by_sensor(y_row, sensor, reach) 
-                for sensor, reach in sensors_reach.items()
+            lambda set1, set2: set1.union(set2),
+            map(partial(row_coverage_by_sensor, y_row), sensors)
+        )
+    )
+
+
+def part1(sensors: list[Sensor], y_row: int) -> None:
+    cov_count_row = count_points_covered_in_row(y_row, sensors)
+    count_not_beacon = cov_count_row - len(
+        set(
+            filter(
+                lambda beacon: beacon.y == y_row,
+                map(lambda sensor: sensor.beacon, sensors)
             )
         )
     )
+    print(f"Number of pos beacon can't be in in row {y_row}:", count_not_beacon)
 
 
 if __name__ == "__main__":
-    sensors_to_beacon = parse_beacons_input("input.txt")
-    sensors_reach = get_sensors_reach(sensors_to_beacon)
-    y_row = 2000000
-    # y_row = 10
-    cov_count_row = count_points_covered_in_row(
-        y_row=y_row, 
-        sensors_reach=sensors_reach
-    )
-    count_occupied = cov_count_row - len(
-        list(
-            filter(
-                lambda beacon: beacon[0] == y_row, 
-                set(sensors_to_beacon.keys())
-            )
-        )
-    )
-    print(count_occupied)
+    sensors = parse_sensor_data("input.txt")
+    part1(sensors, y_row=2_000_000)
