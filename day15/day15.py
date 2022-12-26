2cfc3a010e3862951bd7ae5d0d4f8a9a3cb280e7
Adriano Oliveira <8104069+adri0@users.noreply.github.com>
2022-12-26 22:27:48 +0100

    Solution day15

--- day15/day15.py
+++ day15/day15.py
@@ -1,6 +1,8 @@
-from functools import reduce, partial
-from dataclasses import dataclass
-from typing import NamedTuple
+from functools import partial, reduce
+from operator import add, or_
+from typing import NamedTuple, Optional
+
+from tqdm import tqdm
 
 
 class Point(NamedTuple):
@@ -8,11 +10,15 @@ class Point(NamedTuple):
     y: int
 
 
-@dataclass
-class Sensor:
+class Sensor(NamedTuple):
     pos: Point
     beacon: Point
     reach: int
+
+
+class Interval(NamedTuple):
+    left: int
+    right: int
 
 
 def distance(a: Point, b: Point) -> int:
@@ -38,29 +44,35 @@ def parse_sensor_data(input_path: str) -> list[Sensor]:
     return sensors
 
 
-def row_coverage_by_sensor(y_row: int, sensor: Sensor) -> set[Point]:
+def row_coverage_by_sensor(y_row: int, sensor: Sensor) -> Interval:
     mid_point = Point(sensor.pos.x, y_row)
     dist_mid_point = distance(sensor.pos, mid_point)
     if dist_mid_point > sensor.reach:
-        return set()
-    row_reach = sensor.reach - dist_mid_point 
-    return {
-        Point(x, y_row)
-        for x in range(sensor.pos.x - row_reach, sensor.pos.x + row_reach + 1)
-    }
+        return None
+    row_reach = sensor.reach - dist_mid_point
+    return Interval(
+        left=sensor.pos.x - row_reach,
+        right=sensor.pos.x + row_reach
+    )
 
 
-def count_points_covered_in_row(y_row: int, sensors: list[Sensor]) -> int:
-    return len(
-        reduce(
-            lambda set1, set2: set1.union(set2),
-            map(partial(row_coverage_by_sensor, y_row), sensors)
-        )
+def row_coverage_by_sensor_points(y_row: int, sensor: Sensor) -> set[Point]:
+    match row_coverage_by_sensor(y_row, sensor):
+        case Interval(left, right):
+            return {Point(x, y=y_row) for x in range(left, right + 1)}
+        case None:
+            return set()
+
+
+def points_covered_in_row(y_row: int, sensors: list[Sensor]) -> set[Point]:
+    return reduce(
+        set.union,
+        map(partial(row_coverage_by_sensor_points, y_row), tqdm(sensors))
     )
 
 
 def part1(sensors: list[Sensor], y_row: int) -> None:
-    cov_count_row = count_points_covered_in_row(y_row, sensors)
+    cov_count_row = len(points_covered_in_row(y_row, sensors))
     count_not_beacon = cov_count_row - len(
         set(
             filter(
@@ -72,6 +84,54 @@ def part1(sensors: list[Sensor], y_row: int) -> None:
     print(f"Number of pos beacon can't be in in row {y_row}:", count_not_beacon)
 
 
+def subtract_interval(a: Interval, b: Interval) -> list[Interval]:
+    """ 
+    Subtract from interval a all positions covered by interval b
+    Result a list of intervals corresponding to remaining positions.
+    """
+    if b.left > a.right or b.right < a.left:    # [..a..].[..b..]. -> ......[..a..]..
+        return [a]                               
+    if b.left <= a.left and b.right >= a.right: # [..b..[..a..]..] -> ...............
+        return []
+    if b.left > a.left and b.right < a.right:   # [..a...[.b..]..] -> [..a..].....[a]
+        return [
+            Interval(a.left, b.left - 1), 
+            Interval(b.right + 1, a.right)
+        ]
+    if b.left <= a.left:                        # [..b..]...   
+        return [Interval(b.right + 1, a.right)] # ...[..a..] -> .........[a]
+    if b.right >= a.right:                       
+        return [Interval(a.left, b.left - 1)]
+    raise Exception(f"Missed dealing with case: {a} - {b}")
+
+
+def find_uncovered_point(sensors: list[Sensor], boundary: int) -> Point:
+    for y_row in tqdm(range(boundary)):
+        intervals_covered = map(partial(row_coverage_by_sensor, y_row), sensors)
+        intervals_covered = filter(None, intervals_covered)
+        intervals_remaining = [Interval(left=0, right=boundary)]
+        for sensor_cov in intervals_covered:
+            subtract_sensor_cov = partial(subtract_interval, b=sensor_cov)
+            subtracted_area = map(subtract_sensor_cov, intervals_remaining)
+            intervals_remaining = reduce(add, subtracted_area, [])
+        if intervals_remaining:
+            assert len(intervals_remaining) == 1
+            interval = intervals_remaining[0]
+            assert interval.left == interval.right
+            return Point(x=interval.left, y=y_row)
+    raise Exception("No uncovered point found within boundary")
+
+
+def part2(sensors: list[Sensor], boundary: int):
+    point = find_uncovered_point(sensors, boundary)
+    print("Point distress signal:", point)
+    frequency = 4_000_000 * point.x + point.y
+    print("Frequency distress signal:", frequency)
+
+
 if __name__ == "__main__":
-    sensors = parse_sensor_data("input.txt")
-    part1(sensors, y_row=2_000_000)
+    # path_input, y_row, boundary = "sample_input.txt", 10, 20
+    path_input, y_row, boundary = "input.txt", 2_000_000, 4_000_000
+    sensors = parse_sensor_data(path_input)
+    part1(sensors, y_row)
+    part2(sensors, boundary)
