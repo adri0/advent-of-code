1962bd5658e3980c658cdf9007a6bd9b7e9d9e4a
Adriano Oliveira <8104069+adri0@users.noreply.github.com>
2022-12-31 14:28:45 +0100

    Solution to day12

--- day12/day12.py
+++ day12/day12.py
@@ -1,101 +1,153 @@
-LETTER_CODES = "SabcdefghijklmnopqrstuvwxyzE"
+from dataclasses import dataclass
+from functools import partial
+from typing import Generator, Iterable, NamedTuple, Optional
+
+Map = list[list[int]]
 
 
-def load_map(path):
-    """ Load map from input and convert to number codes """
-    return [list(map(LETTER_CODES.find, line.strip())) for line in open(path)]
+class Coord(NamedTuple):
+    x: int
+    y: int
 
 
-def find_start_end(map_ap):
-    for j in range(len(map_ap)):
-        for i in range(len(map_ap[0])):
-            if map_ap[j][i] == 0:
-                start = (i, j)
-            if map_ap[j][i] == len(LETTER_CODES) - 1:
-                end = (i, j)
-    return start, end
+LETTERS = "abcdefghijklmnopqrstuvwxyz"
 
 
-def walk_next(map_ap, current_pos, visited, end):
-    """ Walk to the next position """
-    options = walk_options(map_ap, current_pos, visited)
-    if not options:
-        visualize(map_ap, current_pos, visited, milestones=[])
-        raise Exception(f"Dead end at {current_pos}")
-    return pick_next_pos(map_ap, options, end)
+def parse_map(input_path: str) -> tuple[Map, Coord, Coord]:
+    """
+    Load map from input file converting chars to elevation numbers
+    and detect start and end coordinates.
+    """
+    map_mat: Map = []
+    for j, line in enumerate(open(input_path)):
+        row = []
+        for i, char in enumerate(line.strip()):
+            match char:
+                case "S":
+                    start = Coord(i, j)
+                    char = "a"
+                case "E":
+                    end = Coord(i, j)
+                    char = "z"
+            row.append(LETTERS.index(char))
+        map_mat.append(row)
+    return map_mat, start, end
 
 
-def distance(a, b):
-    """ Manhattan """
-    x_a, y_a = a
-    x_b, y_b = b
-    return abs(y_b - y_a) + abs(x_b - x_a)
+def within_boundaries(coord: Coord, map_mat: Map) -> bool:
+    return (0 <= coord.x < len(map_mat[0])) and (0 <= coord.y < len(map_mat))
 
 
-def height_diff(map_ap, a, b):
-    (x_a, y_a), (x_b, y_b) = a, b
-    return map_ap[y_b][x_b] - map_ap[y_a][x_a]
+def get_valid_adjacents(
+    map_mat: Map, cur: Coord, visited: set[Coord]
+) -> Iterable[Coord]:
+    """Get valid adjacent coordinates from a given coordinate"""
+    cur_elevation = map_mat[cur.y][cur.x]
+
+    def valid_adjacent(adj: Coord) -> bool:
+        return (
+            within_boundaries(adj, map_mat)
+            and adj not in visited
+            and map_mat[adj.y][adj.x] <= cur_elevation + 1
+        )
+
+    return filter(
+        valid_adjacent,
+        (
+            Coord(cur.x, cur.y + 1),
+            Coord(cur.x + 1, cur.y),
+            Coord(cur.x, cur.y - 1),
+            Coord(cur.x - 1, cur.y),
+        ),
+    )
 
 
-def pick_next_pos(map_ap, walk_options, end):
-    return sorted(
-        walk_options,
-        key=lambda option: distance(option, end) - 10 * height_diff(map_ap, end, option)
-    )[0]
+def colorize(string: str) -> str:
+    END = "\033[0m"
+    COLOR = "\033[95m"
+    BOLD = "\033[1m"
+    return f"{BOLD}{COLOR}{string}{END}"
 
 
-def walk_options(map_ap, current_pos, visited):
-    """ Generate next walk options from the current position """
-    x_cur, y_cur = current_pos
-    height_cur = map_ap[y_cur][x_cur]
-    adjencies = [
-        (x_cur - 1, y_cur),
-        (x_cur + 1, y_cur),
-        (x_cur, y_cur - 1),
-        (x_cur, y_cur + 1)
-    ]
-    options = []
-    for i, j in adjencies:
-        if (
-            (i, j) in visited
-            or i < 0 or j < 0 
-            or i >= len(map_ap[0]) 
-            or j >= len(map_ap)
-        ):
-            continue
-        if map_ap[j][i] in (height_cur, height_cur - 1):
-            options.append((i, j))
-    return options
+def traverse(map_mat: Map) -> Generator[tuple[Coord, int], None, None]:
+    """Traverse the map and yield coordinates and elevation"""
+    for j, row in enumerate(map_mat):
+        for i, elev in enumerate(row):
+            yield Coord(i, j), elev
 
 
-def visualize(map_ap, current_pos, visited, milestones):
-    for j in range(len(map_ap)):
-        for i in range(len(map_ap[0])):
-            if (i, j) == current_pos:
-                print("@", end="")
-            elif (i, j) in visited:
-                print(".", end="")
-            elif (i, j) in milestones:
-                print(LETTER_CODES[map_ap[j][i]].upper(), end="")
-            else:
-                print(LETTER_CODES[map_ap[j][i]], end="")
-        print()
+def visualize(map_ap: Map, current_pos: Coord, visited: Iterable[Coord]) -> None:
+    for coord, elev in traverse(map_mat):
+        if coord == current_pos:
+            print(colorize("*"), end="")
+        elif coord in visited:
+            print(colorize(LETTERS[elev]), end="")
+        else:
+            print(LETTERS[elev], end="")
+        if coord.x == len(map_mat[0]) - 1:
+            print()
     print()
 
-map_ap = load_map("input.txt")
-start, end = find_start_end(map_ap)
 
-# # Going from end to start instead
-current_pos = end
-end = start
+@dataclass
+class PathPos:
+    """Represents a position in a path"""
 
-steps = 0
-visited = set([current_pos])
-milestones = [(145, 23), (152, 26), (155, 15), (142, 29), end]
-for milestone in milestones:
-    while current_pos != milestone:
-        current_pos = walk_next(map_ap, current_pos, visited, milestone)
-        visited.add(current_pos)
-        steps += 1
+    coord: Coord
+    previous: Optional["PathPos"] = None
 
-print("Final destination reached! Steps:", steps)
+
+def breadth_first_search(map_mat: Map, start: Coord, end: Coord) -> Optional[PathPos]:
+    """Breadth-first search to find the shortest path between start to end"""
+    path_start = PathPos(start)
+    queue: list[PathPos] = [path_start]
+    visited: set[Coord] = set([path_start.coord])
+
+    while len(queue) > 0:
+        current = queue.pop(0)
+        if current.coord == end:
+            return current
+        for adj_coord in get_valid_adjacents(map_mat, current.coord, visited):
+            visited.add(adj_coord)
+            next_pos = PathPos(adj_coord)
+            next_pos.previous = current
+            queue.append(next_pos)
+    return None
+
+
+def unroll_path(path_pos: PathPos) -> list[Coord]:
+    """Get a list of coordinates in a path represented by its last position"""
+    path = [path_pos.coord]
+    while path_pos := path_pos.previous:  # type: ignore
+        path.insert(0, path_pos.coord)
+    return path
+
+
+def part1(map_mat: list[list[int]], start: Coord, end: Coord) -> None:
+    """Print minimum number of steps from start to end"""
+    path_pos = breadth_first_search(map_mat, start, end)
+    if not path_pos:
+        raise Exception("No path found!")
+    path = unroll_path(path_pos)
+    visualize(map_mat, end, path)
+    n_steps = len(path) - 1
+    print(f"Minimum number of steps from start at {start}:", n_steps)
+
+
+def part2(map_mat: list[list[int]], end: Coord) -> None:
+    """Print minimum number of steps from any 'a' elevation to end"""
+    elev_at_a = LETTERS.index("a")
+    start = (coord for coord, elev in traverse(map_mat) if elev == elev_at_a)
+    path_pos = map(partial(breadth_first_search, map_mat, end=end), start)
+    path_pos_found = filter(None, path_pos)
+    paths = map(unroll_path, path_pos_found)
+    shortest_path = min(paths, key=len)
+    visualize(map_mat, end, set(shortest_path))
+    n_steps = len(shortest_path) - 1
+    print("Minimum number of steps from any 'a' elevation:", n_steps)
+
+
+if __name__ == "__main__":
+    map_mat, start, end = parse_map("input.txt")
+    part1(map_mat, start, end)
+    part2(map_mat, end)
