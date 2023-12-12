6230190034cbb9e2d059116db67afb402dcd6f9f
Adriano Oliveira <8104069+adri0@users.noreply.github.com>
2023-12-12 23:10:49 +0100

    Solution for day 11 part 2

--- 2023/11/day11.py
+++ 2023/11/day11.py
@@ -1,8 +1,8 @@
 from itertools import combinations
-from typing import Any, NamedTuple
+from typing import NamedTuple
 
 import numpy as np
-import pandas as pd
+from pandas import DataFrame
 
 EMPTY = "."
 GALAXY = "#"
@@ -13,42 +13,39 @@ class Point(NamedTuple):
     y: int
 
 
-def read_image(input_path: str) -> pd.DataFrame:
+def read_image(input_path: str) -> DataFrame:
     with open(input_path) as f:
-        lines = [list(line.strip()) for line in f.readlines()]
-    return pd.DataFrame(lines)
+        return DataFrame([list(line.strip()) for line in f])
 
 
-def expand_empty(image: pd.DataFrame) -> pd.DataFrame:
-    rows: list[Any] = []
-    empty_row = [EMPTY] * len(image.columns)
-    for _, row in image.copy().iterrows():
-        rows.append(row.values)
-        if all(pixel == EMPTY for pixel in row):
-            for i in range(1_000_000):
-                rows.append(empty_row)
-    image = pd.DataFrame(rows)
-
-    for col in image.copy().columns:
-        if all(image.loc[:, col] == EMPTY):
-            for i in range(1_000_000):
-                image.insert(
-                    loc=image.columns.get_loc(col), column=f"{col}'", value=EMPTY
-                )
-    image.columns = range(len(image.columns))
-
-    return image
+class EmptySpace(NamedTuple):
+    rows: list[int]
+    cols: list[int]
 
 
-def part1(image: pd.DataFrame) -> None:
-    points = list(Point(x, y) for x, y in zip(*np.where(image == GALAXY)))
-    distances = [abs(a.x - b.x) + abs(a.y - b.y) for a, b in combinations(points, 2)]
-    result = sum(distances)
-    print("Sum of distance:", result)
+def expand_point(point: Point, expansion: int, empty_space: EmptySpace) -> Point:
+    x_shift = (expansion - 1) * len([row for row in empty_space.rows if point.x > row])
+    y_shift = (expansion - 1) * len([col for col in empty_space.cols if point.y > col])
+    return Point(point.x + x_shift, point.y + y_shift)
+
+
+def sum_distances(image: DataFrame, empty_space: EmptySpace, expansion: int) -> int:
+    points = (Point(x, y) for x, y in zip(*np.where(image == GALAXY)))
+    points_x = map(lambda p: expand_point(p, expansion, empty_space), points)
+    distances = (abs(a.x - b.x) + abs(a.y - b.y) for a, b in combinations(points_x, 2))
+    return sum(distances)
 
 
 if __name__ == "__main__":
     image = read_image("input.txt")
-    image = expand_empty(image)
-    print(image)
-    part1(image)
+
+    empty_space = EmptySpace(
+        rows=[i for i, row in image.iterrows() if all(pixel == EMPTY for pixel in row)],
+        cols=[int(col) for col in image.columns if all(image[col] == EMPTY)],
+    )
+
+    part1_res = sum_distances(image, empty_space, expansion=2)
+    print("(Part 1) Distances sum:", part1_res)
+
+    part2_res = sum_distances(image, empty_space, expansion=1_000_000)
+    print("(Part 2) Distances sum:", part2_res)
