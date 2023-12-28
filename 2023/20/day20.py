85f874713d409d104e61d6ae284d81320c1477a0
Adriano Oliveira <8104069+adri0@users.noreply.github.com>
2023-12-28 11:13:52 +0100

    Day 20 part 1

--- 2023/20/day20.py
+++ 2023/20/day20.py
@@ -1,101 +1,122 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from enum import Enum
-from typing import Optional
+from typing import NamedTuple, cast
 
 
-class Pulse(Enum):
-    LOW = 1
-    HIGH = 2
+class Intensity(Enum):
+    LOW = 0
+    HIGH = 1
+
+
+class Pulse(NamedTuple):
+    source: str
+    target: str
+    intensity: Intensity
 
 
 @dataclass
 class Module(ABC):
     name: str
-    output: list[str]
-
-    _nlow: int = 0
-    _nhigh: int = 0
-    _pulse_to_send: Optional[Pulse] = None
+    out_connections: list[str]
 
     @abstractmethod
-    def receive_pulse(self, name: str, pulse: Pulse) -> None:
+    def receive_pulse(self, pulse: Pulse) -> list[Pulse]:
         raise NotImplemented
 
-    # @abstractmethod
-    def send_pulse(self, modules: dict[str, "Module"]) -> None:
-        for module in self.output:
-            if self._pulse_to_send:
-                if self._pulse_to_send == Pulse.HIGH:
-                    self._nhigh += 1
-                else:
-                    self._nlow += 1
-                modules[module].receive_pulse(self.name, self._pulse_to_send)
-        for module in self.output:
-            if modules[module]._pulse_to_send:
-                modules[module].send_pulse(modules)
+    def generate_pulses(self, intensity: Intensity) -> list[Pulse]:
+        return [
+            Pulse(source=self.name, target=module, intensity=intensity)
+            for module in self.out_connections
+        ]
 
 
 @dataclass
 class FlipFlop(Module):
     on: bool = False
 
-    def receive_pulse(self, name: str, pulse: Pulse) -> None:
-        if pulse == Pulse.LOW:
-            self._pulse_to_send = Pulse.LOW if self.on else Pulse.HIGH
-            self.on = not self.on
+    def receive_pulse(self, pulse: Pulse) -> list[Pulse]:
+        if pulse.intensity == Intensity.HIGH:
+            return []
         else:
-            self._pulse_to_send = None
+            out_intensity = Intensity.LOW if self.on else Intensity.HIGH
+            self.on = not self.on
+            return self.generate_pulses(out_intensity)
 
 
 @dataclass
 class Conjunction(Module):
-    memory: dict[str, Pulse] = field(default_factory=dict)
+    memory: dict[str, Intensity] = field(default_factory=dict)
 
-    def receive_pulse(self, name: str, pulse: Pulse) -> None:
-        self.memory[name] = pulse
-        if all(pulse == Pulse.HIGH for pulse in self.memory.values()):
-            self._pulse_to_send = Pulse.LOW
-        else:
-            self._pulse_to_send = Pulse.HIGH
+    def receive_pulse(self, pulse: Pulse) -> list[Pulse]:
+        self.memory[pulse.source] = pulse.intensity
+        out_intensity = (
+            Intensity.LOW
+            if all(intensity == Intensity.HIGH for intensity in self.memory.values())
+            else Intensity.HIGH
+        )
+        return self.generate_pulses(out_intensity)
 
 
-@dataclass
 class Broadcaster(Module):
-    def receive_pulse(self, name: str, pulse: Pulse) -> None:
-        self._pulse_to_send = pulse
+    def receive_pulse(self, pulse: Pulse) -> list[Pulse]:
+        return self.generate_pulses(pulse.intensity)
+
+
+class Output(Module):
+    def __init__(self, name: str) -> None:
+        super().__init__(name, out_connections=[])
+
+    def receive_pulse(self, pulse: Pulse) -> list[Pulse]:
+        return []
+
+
+def parse_module(input_line: str) -> Module:
+    name, connect_str = input_line.strip().split(" -> ")
+    out_modules = connect_str.split(", ")
+    module: Module
+    if name == "broadcaster":
+        module = Broadcaster(name, out_modules)
+    elif name.startswith("%"):
+        module = FlipFlop(name[1:], out_modules)
+    elif name.startswith("&"):
+        module = Conjunction(name[1:], out_modules)
+    else:
+        raise ValueError("Shouldn't be here")
+    return module
 
 
 def read_modules(input_path: str) -> dict[str, Module]:
     with open(input_path) as f:
-        modules = {}
-        for line in f:
-            name, output_str = line.strip().split(" -> ")
-            # type_str = name[0] if name != "broadcaster" else "broadcaster"
-            output = output_str.split(", ")
-            module: Module
-            if name == "broadcaster":
-                module = Broadcaster(name, output)
-            elif name.startswith("%"):
-                module = FlipFlop(name[1:], output)
-            elif name.startswith("&"):
-                module = Conjunction(name[1:], output)
-            else:
-                raise ValueError("Shouldn't be here")
-            modules[module.name] = module
-    # Populate input from output
-    for name, module in modules.items():
-        for out in module.output:
-            match modules[out]:
-                case Conjunction() as conjunction:
-                    conjunction.memory[name] = Pulse.LOW
+        modules = {module.name: module for module in map(parse_module, f)}
+
+    # Identify output modules and populate memory for conjunctions
+    out_modules = {}
+    for module in modules.values():
+        for out in module.out_connections:
+            if out not in modules and out not in out_modules:
+                out_modules[out] = Output(name=out)
+            elif isinstance(modules[out], Conjunction):
+                cast(Conjunction, modules[out]).memory[module.name] = Intensity.LOW
+    modules.update(out_modules)
 
     return modules
 
 
+def part1(modules: dict[str, Module]) -> None:
+    lows = highs = 0
+    for _ in range(1000):
+        pulses = [Pulse(source="button", target="broadcaster", intensity=Intensity.LOW)]
+        while pulses:
+            pulse = pulses.pop()
+            out_pulses = modules[pulse.target].receive_pulse(pulse)
+            pulses = out_pulses + pulses
+            highs += int(pulse.intensity == Intensity.HIGH)
+            lows += int(pulse.intensity == Intensity.LOW)
+
+    print("(Part 1) Result:", lows * highs)
+
+
 if __name__ == "__main__":
-    modules: dict[str, Module] = read_modules("example1.txt")
-    modules["broadcaster"].receive_pulse("button", Pulse.LOW)
-    modules["broadcaster"].send_pulse(modules)
-    print("(Part 1) Pulses", sum(m._nhigh for m in modules.values()))
-    print("(Part 1) Pulses", sum(m._nlow for m in modules.values()))
+    modules = read_modules("input.txt")
+    part1(modules)
