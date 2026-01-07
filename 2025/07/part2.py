0ef0550e9f4c8a5fc4b734abd6d0e14f9b0d36e3
Adriano Oliveira <8104069+adri0@users.noreply.github.com>
2026-01-07 18:24:39 +0100

    Update part2

--- 2025/07/part2.py
+++ 2025/07/part2.py
@@ -1,20 +1,18 @@
 with open("input.txt") as f:
-    # Read grid replacing dots by zero
+    # Read grid replacing dots by zero and S by 1
     grid: list[list[str | int]] = [
-        [{".": 0}.get(char, char) for char in line] for line in f.read().strip().split()
+        [{".": 0, "S": 1}.get(char, char) for char in line]
+        for line in f.read().strip().split()
     ]
 
 for i in range(1, len(grid)):
     for j in range(len(grid[0])):
-        match grid[i - 1][j]:
-            case "S":
-                grid[i][j] = 1  # number of timelines at position
-            case int(n_timelines):
-                if grid[i][j] == "^":
-                    grid[i][j - 1] += n_timelines
-                    grid[i][j + 1] += n_timelines
-                else:
-                    grid[i][j] += n_timelines
+        if (previous := grid[i - 1][j]) != "^":
+            if grid[i][j] == "^":
+                grid[i][j - 1] += previous
+                grid[i][j + 1] += previous
+            else:
+                grid[i][j] += previous
 
 timelines = sum(v for v in grid[i] if isinstance(v, int))
 
